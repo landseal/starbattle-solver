8df183e49dab7946d229a1178d6428e1c08e864c
Daniel Sealand <daniel.c.sealand@gmail.com>
2022-12-28 13:15:14 -0800

    valid solution helper function, 8x8 puzzle

--- starbattle.py
+++ starbattle.py
@@ -1,12 +1,28 @@
-arealist = [
-    [1,1,1,2,2,],
-    '13332',
-    '44422',
-    '44422',
-    '55555',
-]
-mapsize = 5
+import copy
 
+#
+v1b2p4x8 = [
+    [1,1,1,1,2,3,3,3],
+    [1,1,3,3,2,3,3,3],
+    [1,1,3,3,3,3,4,3],
+    [1,5,6,3,4,4,4,4],
+    [1,5,6,3,6,4,4,4],
+    [7,7,6,6,6,4,4,4],
+    [7,7,6,6,6,6,4,4],
+    [7,7,6,8,8,6,6,6]
+]
+mapsize = 8
+
+blanklol = [
+    [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
+    [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
+    [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
+    [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
+    [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
+    [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
+    [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
+    [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
+]
 def blankmap(size):
     lol = []
     for row in range(size):
@@ -16,27 +32,85 @@ def blankmap(size):
         lol += [helper]
     return lol
 
-def checkvalid(areas, starmap):
+def validsolution(areas, starmap):
+    starmapsize = len(starmap)
+    # check no Xs next to each other
+    for row in range(starmapsize):
+        for col in range(starmapsize):
+            if starmap[row][col] == 'X':
+                if row != starmapsize-1:
+                    # check square below
+                    if starmap[row+1][col] == 'X': return False
+                if col != starmapsize-1:
+                    # check square to right
+                    if starmap[row][col+1] == 'X': return False
+                if row != starmapsize-1 and col != starmapsize-1:
+                    # check square below and right
+                    if starmap[row+1][col+1] == 'X': return False
+                if row != starmapsize-1 and col != 0:
+                    # check square below and left
+                    if starmap[row+1][col-1] == 'X': return False
+    
+    # check that every column and row has exactly 1 X
+    for row in range(starmapsize):
+        rowcount = 0
+        for col in range(starmapsize):
+            if starmap[row][col] == 'X':
+                rowcount += 1
+        if rowcount != 1: return False
+    for col in range(starmapsize):
+        colcount = 0
+        for row in range(starmapsize):
+            if starmap[row][col] == 'X':
+                colcount += 1
+        if colcount != 1: return False
+
+    # check if every area has exactly 1 X
+    for areaindex in range(mapsize):
+        count = 0
+        for row in range(starmapsize):
+            for col in range(starmapsize):
+                if areas[row][col] == areaindex and starmap[row][col] == 'X':
+                    count += 1
+        if count != 1:
+            return False
+    return True
+
+def checkvalid(starmap):
     starmapsize = len(starmap)
     starmaprange = range(starmapsize)
     # check no Xs next to each other
-    validity = True
+    # validity = True
     for row in starmaprange:
         for col in starmaprange:
             if starmap[row][col] == 'X':
                 if row != starmapsize-1:
                     # check square below
-                    if starmap[row+1][col] == 'X': validity = False
+                    if starmap[row+1][col] == 'X': return False
                 if col != starmapsize-1:
                     # check square to right
-                    if starmap[row][col+1] == 'X': validity = False
+                    if starmap[row][col+1] == 'X': return False
                 if row != starmapsize-1 and col != starmapsize-1:
                     # check square below and right
-                    if starmap[row+1][col+1] == 'X': validity = False
+                    if starmap[row+1][col+1] == 'X': return False
                 if row != starmapsize-1 and col != 0:
                     # check square below and left
-                    if starmap[row+1][col-1] == 'X': validity = False
-    return validity
+                    if starmap[row+1][col-1] == 'X': return False
+    
+    # check if any column or row has more than 2 Xs
+    for row in starmaprange:
+        rowcount = 0
+        for col in starmaprange:
+            if starmap[row][col] == 'X':
+                rowcount += 1
+        if rowcount > 1: return False
+    for col in starmaprange:
+        colcount = 0
+        for row in starmaprange:
+            if starmap[row][col] == 'X':
+                colcount += 1
+        if colcount > 1: return False
+    return True
                     
 
 def checkfull(starmap):
@@ -46,16 +120,14 @@ def checkfull(starmap):
     return True
 
 def addsymbol(starmap, symbol):
+    copymap = copy.deepcopy(starmap)
     starmapsize = len(starmap)
     starmaprange = range(starmapsize)
-    notadded = True
     for row in starmaprange:
         for col in starmaprange:
             if starmap[row][col] == ' ':
-                if notadded:
-                    starmap[row][col] = symbol
-                    notadded = False
-    return starmap
+                copymap[row][col] = symbol
+                return copymap
 
 def backtonotstar(starmap):
     starmapsize = len(starmap)
@@ -76,22 +148,20 @@ def backtonotstar(starmap):
 
 
 def recursivesolver(areas, starmap):
-    # print('hi')
     for row in starmap:
         print(row)
-    print(' ')
-    if checkfull(starmap) and checkvalid(areas, starmap):
+    print('')
+    if checkfull(starmap) and validsolution(v1b2p4x8, starmap):
         print('valid')
         for row in starmap:
             print(row)
+        print('valid')
         return True
-    elif not checkvalid(areas, starmap):
+    elif not checkvalid(starmap):
         print('invalid')
         return False
     else:
-        # addstar = recursivesolver(areas, addsymbol(starmap, 'X'))
-        # addnotstar = recursivesolver(areas, addsymbol(starmap, '-'))
-        recursivesolver(areas, addsymbol(starmap, '-'))
         recursivesolver(areas, addsymbol(starmap, 'X'))
+        recursivesolver(areas, addsymbol(starmap, '-'))
 
-recursivesolver(arealist, blankmap(5))
+recursivesolver(v1b2p4x8, blanklol)
