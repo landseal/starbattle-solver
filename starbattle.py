4e34f05e749ee6401e9ec701d06f8228a333ad04
Joshua Sealand <daniel.c.sealand@gmail.com>
2022-12-28 22:36:08 -0800

    deepcopy is mega slow

--- starbattle.py
+++ starbattle.py
@@ -1,7 +1,8 @@
 import copy
 import time
+import ujson
 
-#
+recursivecounter = 0
 v1b2p4x8 = [
     [1,1,1,1,2,3,3,3],
     [1,1,3,3,2,3,3,3],
@@ -59,6 +60,7 @@ blanklol = [
     [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
     [' ', ' ', ' ', ' ', ' ',' ',' ',' '],
 ]
+
 def blankmap(size):
     lol = []
     for row in range(size):
@@ -71,35 +73,35 @@ def blankmap(size):
 def validsolution(areas, starmap, areacount, starcount):
     starmapsize = len(starmap)
     # check no Xs next to each other
-    for row in range(starmapsize):
-        for col in range(starmapsize):
-            if starmap[row][col] == 'X':
-                if row != starmapsize-1:
-                    # check square below
-                    if starmap[row+1][col] == 'X': return False
-                if col != starmapsize-1:
-                    # check square to right
-                    if starmap[row][col+1] == 'X': return False
-                if row != starmapsize-1 and col != starmapsize-1:
-                    # check square below and right
-                    if starmap[row+1][col+1] == 'X': return False
-                if row != starmapsize-1 and col != 0:
-                    # check square below and left
-                    if starmap[row+1][col-1] == 'X': return False
+    # for row in range(starmapsize):
+    #     for col in range(starmapsize):
+    #         if starmap[row][col] == 'X':
+    #             if row != starmapsize-1:
+    #                 # check square below
+    #                 if starmap[row+1][col] == 'X': return False
+    #             if col != starmapsize-1:
+    #                 # check square to right
+    #                 if starmap[row][col+1] == 'X': return False
+    #             if row != starmapsize-1 and col != starmapsize-1:
+    #                 # check square below and right
+    #                 if starmap[row+1][col+1] == 'X': return False
+    #             if row != starmapsize-1 and col != 0:
+    #                 # check square below and left
+    #                 if starmap[row+1][col-1] == 'X': return False
     
     # check that every column and row has exactly starcount Xs
-    for row in range(starmapsize):
-        rowcount = 0
-        for col in range(starmapsize):
-            if starmap[row][col] == 'X':
-                rowcount += 1
-        if rowcount != starcount: return False
-    for col in range(starmapsize):
-        colcount = 0
-        for row in range(starmapsize):
-            if starmap[row][col] == 'X':
-                colcount += 1
-        if colcount != starcount: return False
+    # for row in range(starmapsize):
+    #     rowcount = 0
+    #     for col in range(starmapsize):
+    #         if starmap[row][col] == 'X':
+    #             rowcount += 1
+    #     if rowcount != starcount: return False
+    # for col in range(starmapsize):
+    #     colcount = 0
+    #     for row in range(starmapsize):
+    #         if starmap[row][col] == 'X':
+    #             colcount += 1
+    #     if colcount != starcount: return False
 
     # check if every area has exactly starcount Xs
     for areaindex in range(areacount):
@@ -112,6 +114,15 @@ def validsolution(areas, starmap, areacount, starcount):
             return False
 
     return True
+
+def checkfullmap(starmap, starcount):
+    maxstarcount = starcount * len(starmap)
+    count = 0
+    for row in starmap:
+        for i in row:
+            if i == 'X': count += 1
+    return count == maxstarcount
+
 
 def checkvalid(starmap, starcount):
     starmapsize = len(starmap)
@@ -157,7 +168,7 @@ def checkfull(starmap):
     return True
 
 def addsymbol(starmap, symbol):
-    copymap = copy.deepcopy(starmap)
+    copymap = ujson.loads(ujson.dumps(starmap))
     starmapsize = len(starmap)
     starmaprange = range(starmapsize)
     for row in starmaprange:
@@ -166,10 +177,11 @@ def addsymbol(starmap, symbol):
                 copymap[row][col] = symbol
                 return copymap
 
-def smartaddstar(starmap, symbol='X'):
-    copymap = copy.deepcopy(starmap)
+def smartaddstar(areas, starmap, symbol='X'):
+    copymap = ujson.loads(ujson.dumps(starmap))
     starmapsize = len(starmap)
     starmaprange = range(starmapsize)
+
     for row in starmaprange:
         for col in starmaprange:
             if starmap[row][col] == ' ':
@@ -198,7 +210,11 @@ def smartaddstar(starmap, symbol='X'):
                 if col != starmapsize-1:
                     if copymap[row][col+1] == ' ': copymap[row][col+1] = '-'
                 # set area to -
-                
+                areanumber = areas[row][col]
+                for i in starmaprange:
+                    for j in starmaprange:
+                        if areas[row][col] == areanumber:
+                            if copymap[row][col] == ' ': copymap[row][col] = '-'
 
                 return copymap
 
@@ -221,12 +237,15 @@ def backtonotstar(starmap):
 
 
 def recursivesolver(areas, starmap, starcount, debug=False):
+    global recursivecounter
+    recursivecounter += 1
     if debug:
         for row in starmap:
             print(row)
         print('')
     if checkfull(starmap):
         if validsolution(areas, starmap, len(starmap), starcount):
+        # if checkfullmap(starmap, starcount):
             print('valid')
             for row in starmap:
                 print(row)
@@ -235,12 +254,12 @@ def recursivesolver(areas, starmap, starcount, debug=False):
             return True
         else:
             return False
-    elif not checkvalid(starmap, starcount):
-        # print('invalid')
-        return False
+    # elif not checkvalid(starmap, starcount):
+    #     # print('invalid')
+    #     return False
     else:
         # recursivesolver(areas, addsymbol(starmap, 'X'), starcount, debug)
-        recursivesolver(areas, smartaddstar(starmap), starcount, debug)
+        recursivesolver(areas, smartaddstar(areas, starmap), starcount, debug)
         recursivesolver(areas, addsymbol(starmap, '-'), starcount, debug)
 
 def starbattlesolver(areas, starcount=0, debug=False):
@@ -254,5 +273,8 @@ def starbattlesolver(areas, starcount=0, debug=False):
     print(starcount)
     areacount = len(areas)*starcount
     starttime = time.time()
-    recursivesolver(areas, blankmap(len(areas)), starcount)
+    global recursivecounter
+    recursivecounter = 0
+    recursivesolver(areas, blankmap(len(areas)), starcount, debug)
     print('elapsed time is', time.time()-starttime, 's')
+    print(recursivecounter)
