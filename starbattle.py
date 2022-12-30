b90e880d91614aca2d1d2bc0f7a11cb455b15efb
Joshua Sealand <daniel.c.sealand@gmail.com>
2022-12-29 22:59:24 -0800

    Optimizing addstar and order of adding stars

--- starbattle.py
+++ starbattle.py
@@ -123,7 +123,6 @@ def checkfullmap(starmap, starcount):
             if i == 'X': count += 1
     return count == maxstarcount
 
-
 def checkvalid(starmap, starcount):
     starmapsize = len(starmap)
     starmaprange = range(starmapsize)
@@ -159,7 +158,6 @@ def checkvalid(starmap, starcount):
                 colcount += 1
         if colcount > starcount: return False
     return True
-                    
 
 def checkfull(starmap):
     # print(starmap)
@@ -167,31 +165,97 @@ def checkfull(starmap):
         if ' ' in row: return False
     return True
 
-def addsymbol(starmap, symbol):
+def addsymbol(starmap, symbol, orderedcoordinates):
     copymap = ujson.loads(ujson.dumps(starmap))
     starmapsize = len(starmap)
     starmaprange = range(starmapsize)
-    for row in starmaprange:
-        for col in starmaprange:
-            if starmap[row][col] == ' ':
-                copymap[row][col] = symbol
-                return copymap
+    # for row in starmaprange:
+    #     for col in starmaprange:
+    for row, col in orderedcoordinates:
+        if starmap[row][col] == ' ':
+            copymap[row][col] = symbol
+            return copymap
 
-def smartaddstar(areas, starmap, symbol='X'):
+def smartaddstar(areas, starmap, starcount, orderedcoordinates, symbol='X'):
     copymap = ujson.loads(ujson.dumps(starmap))
     starmapsize = len(starmap)
     starmaprange = range(starmapsize)
 
-    for row in starmaprange:
-        for col in starmaprange:
-            if starmap[row][col] == ' ':
-                copymap[row][col] = 'X'
-                # set row to -
+    fullnessdict = {
+        'rowcount': [0]*starmapsize,
+        'colcount': [0]*starmapsize,
+        'areacount': [0]*starmapsize,
+    }
+
+    for row, col in orderedcoordinates:
+        if starmap[row][col] in ' X':
+            fullnessdict['rowcount'][row] += 1
+            fullnessdict['colcount'][col] += 1
+            fullnessdict['areacount'][areas[row][col]] += 1
+        if starmap[row][col] == ' ':
+            copymap[row][col] = 'X'
+            # if row is full, set rest to -
+            if fullnessdict['rowcount'][row] == starcount:
                 for i in starmaprange:
                     if copymap[row][i] == ' ': copymap[row][i] = '-'
-                # set col to -
+            # if col is full, set rest to -
+            if fullnessdict['colcount'][col] == starcount:
                 for i in starmaprange:
                     if copymap[i][col] == ' ': copymap[i][col] = '-'
+            # set around to -
+            if row != 0:
+                if copymap[row-1][col] == ' ': copymap[row-1][col]='-'
+                if col != 0:
+                    if copymap[row-1][col-1] == ' ': copymap[row-1][col-1] = '-'
+                if col != starmapsize-1:
+                    if copymap[row-1][col+1] == ' ': copymap[row-1][col+1] = '-'
+            if row != starmapsize-1:
+                if copymap[row+1][col] == ' ': copymap[row+1][col] = '-'
+                if col != 0:
+                    if copymap[row+1][col-1] == ' ': copymap[row+1][col-1] = '-'
+                if col != starmapsize-1:
+                    if copymap[row+1][col+1] == ' ': copymap[row+1][col+1] = '-'
+            if col != 0:
+                if copymap[row][col-1] == ' ': copymap[row][col-1] = '-'
+            if col != starmapsize-1:
+                if copymap[row][col+1] == ' ': copymap[row][col+1] = '-'
+            # if area is full, set area to -
+            areanumber = areas[row][col]
+            if fullnessdict['areacount'][areanumber] == starcount:
+                for i in starmaprange:
+                    for j in starmaprange:
+                        if areas[i][j] == areanumber:
+                            if copymap[i][j] == ' ': copymap[i][j] = '-'
+
+            return copymap
+
+def oldsmartaddstar(areas, starmap, starcount, symbol='X'):
+    copymap = ujson.loads(ujson.dumps(starmap))
+    starmapsize = len(starmap)
+    starmaprange = range(starmapsize)
+
+    fullnessdict = {
+        'rowcount': [0]*starmapsize,
+        'colcount': [0]*starmapsize,
+        'areacount': [0]*starmapsize,
+    }
+
+    for row in starmaprange:
+        for col in starmaprange:
+            if starmap[row][col] in ' X':
+                fullnessdict['rowcount'][row] += 1
+                fullnessdict['colcount'][col] += 1
+                fullnessdict['areacount'][areas[row][col]] += 1
+            if starmap[row][col] == ' ':
+                copymap[row][col] = 'X'
+                # if row is full, set rest to -
+                if fullnessdict['rowcount'][row] == starcount:
+                    for i in starmaprange:
+                        if copymap[row][i] == ' ': copymap[row][i] = '-'
+                # if col is full, set rest to -
+                if fullnessdict['colcount'][col] == starcount:
+                    for i in starmaprange:
+                        if copymap[i][col] == ' ': copymap[i][col] = '-'
                 # set around to -
                 if row != 0:
                     if copymap[row-1][col] == ' ': copymap[row-1][col]='-'
@@ -209,14 +273,16 @@ def smartaddstar(areas, starmap, symbol='X'):
                     if copymap[row][col-1] == ' ': copymap[row][col-1] = '-'
                 if col != starmapsize-1:
                     if copymap[row][col+1] == ' ': copymap[row][col+1] = '-'
-                # set area to -
+                # if area is full, set area to -
                 areanumber = areas[row][col]
-                for i in starmaprange:
-                    for j in starmaprange:
-                        if areas[row][col] == areanumber:
-                            if copymap[row][col] == ' ': copymap[row][col] = '-'
+                if fullnessdict['areacount'][areanumber] == starcount:
+                    for i in starmaprange:
+                        for j in starmaprange:
+                            if areas[i][j] == areanumber:
+                                if copymap[i][j] == ' ': copymap[i][j] = '-'
 
                 return copymap
+
 
 def backtonotstar(starmap):
     starmapsize = len(starmap)
@@ -235,17 +301,40 @@ def backtonotstar(starmap):
                 prevcol = col
     return starmap
 
+def orderbyareasize(areas):
+    areaslength = len(areas)
+    areasizelist = []
+    for i in range(areaslength):
+        areasizelist += [[0, i]]
+    # print(areasizelist)
+    for row in range(areaslength):
+        for col in range(areaslength):
+            areasizelist[areas[row][col]][0] += 1
+    # print(areasizelist)
+    areasizelist.sort()
+    # print(areasizelist)
+    orderedcoordinatelist = []
+    for areacount, area in areasizelist:
+        for row in range(areaslength):
+            for col in range(areaslength):
+                if areas[row][col] == area:
+                    orderedcoordinatelist += [[row, col]]
+    return orderedcoordinatelist
 
-def recursivesolver(areas, starmap, starcount, debug=False):
+def recursivesolver(areas, starmap, starcount, orderedcoordinates, debug=False):
     global recursivecounter
     recursivecounter += 1
-    if debug:
+    if debug or (recursivecounter%200000 == 0):
         for row in starmap:
-            print(row)
+            a = ''
+            for i in row:
+                a += str(i) + ' '
+            print(a)
         print('')
+        time.sleep(1)
     if checkfull(starmap):
-        if validsolution(areas, starmap, len(starmap), starcount):
-        # if checkfullmap(starmap, starcount):
+        # if validsolution(areas, starmap, len(starmap), starcount):
+        if checkfullmap(starmap, starcount):
             print('valid')
             for row in starmap:
                 print(row)
@@ -259,10 +348,14 @@ def recursivesolver(areas, starmap, starcount, debug=False):
     #     return False
     else:
         # recursivesolver(areas, addsymbol(starmap, 'X'), starcount, debug)
-        recursivesolver(areas, smartaddstar(areas, starmap), starcount, debug)
-        recursivesolver(areas, addsymbol(starmap, '-'), starcount, debug)
+        return recursivesolver(
+            areas, smartaddstar(areas, starmap, starcount, orderedcoordinates), starcount, orderedcoordinates, debug
+        ) or recursivesolver(
+            areas, addsymbol(starmap, '-', orderedcoordinates), starcount, orderedcoordinates, debug
+        )
 
 def starbattlesolver(areas, starcount=0, debug=False):
+    # infer starcount if it's set to 0
     if starcount == 0:
         if len(areas) < 10:
             starcount = 1
@@ -270,11 +363,16 @@ def starbattlesolver(areas, starcount=0, debug=False):
             starcount = 2
         else:
             starcount = 3
-    print(starcount)
-    areacount = len(areas)*starcount
+    # print(starcount)
+
+    # calculate order to add stars
+    orderedcoordinates = orderbyareasize(areas)
+
     starttime = time.time()
     global recursivecounter
     recursivecounter = 0
-    recursivesolver(areas, blankmap(len(areas)), starcount, debug)
+
+    recursivesolver(areas, blankmap(len(areas)), starcount, orderedcoordinates, debug)
+
     print('elapsed time is', time.time()-starttime, 's')
     print(recursivecounter)
